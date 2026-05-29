4cc0f66147a25137aeaaff4e50e1a02c341392e1
Dea Harun <deaharun@MacBook-Pro-Dea.local>
2026-05-29 18:24:15 +0700

    fix(vercel): remove conflicting domain redirect

--- next.config.js
+++ next.config.js
@@ -20,21 +20,6 @@ const nextConfig = {
         ]
       }
     ];
-  },
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.andiwahyudi.com"
-          }
-        ],
-        destination: "https://andiwahyudi.com/:path*",
-        permanent: true
-      }
-    ];
   }
 };
 
